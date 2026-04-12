a4c9cedc55faf119d44ca7452d4a784e47740bd2
explrprobe <explrprobe@gmail.com>
2026-04-12 14:58:20 +0700

    refactoring tests for test_news api news_list and view_news

--- tests/api/test_news.py
+++ tests/api/test_news.py
@@ -1,35 +1,46 @@
 import pytest
 from helpers import get_csrf_token
 
-def test_news_unauthenticated_user_get(unauthenticated_session, base_url, test_news):
-    """Unauthenticated user can see news"""
+#API news_list
+def test_news_list_unauthenticated_user(unauthenticated_session, base_url, test_news):
+    """Unauthenticated user can see news list"""
     response_news_list = unauthenticated_session.get(url=f"{base_url}news/")
     assert response_news_list.status_code == 200, "Unauthenticated user should have access to news list"
     assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_news_list_regular_user(regular_session, base_url, test_news):
+    """Unauthenticated user can see news list"""
+    response_news_list = regular_session.get(url=f"{base_url}news/")
+    assert response_news_list.status_code == 200, "Unauthenticated user should have access to news list"
+    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_news_list_editor_user(editor_session, base_url, test_news):
+    """Unauthenticated user can see news list"""
+    response_news_list = editor_session.get(url=f"{base_url}news/")
+    assert response_news_list.status_code == 200, "Unauthenticated user should have access to news list"
+    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_news_list_admin_user(admin_session, base_url, test_news):
+    """Unauthenticated user can see news list"""
+    response_news_list = admin_session.get(url=f"{base_url}news/")
+    assert response_news_list.status_code == 200, "Unauthenticated user should have access to news list"
+    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+
+#API view_news
+def test_view_news_unauthenticated_user(unauthenticated_session, test_news):
+    """Unauthenticated user can see news details"""
     response_news = unauthenticated_session.get(url=test_news["test_news_url"])
     assert response_news.status_code == 200, "Unauthenticated user should have access to news"
     assert test_news["test_news_data"]["title"] in response_news.text, "Created news is not accessable"
-def test_news_regular_user_get(regular_session, base_url, test_news):
-    """Regular user can see news"""
-    response_news_list = regular_session.get(url=f"{base_url}news/")
-    assert response_news_list.status_code == 200, "Regular user should have access to news list"
-    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_view_news_regular_user(regular_session, test_news):
+    """Unauthenticated user can see news details"""
     response_news = regular_session.get(url=test_news["test_news_url"])
-    assert response_news.status_code == 200, "Regular user should have access to news"
+    assert response_news.status_code == 200, "Unauthenticated user should have access to news"
     assert test_news["test_news_data"]["title"] in response_news.text, "Created news is not accessable"
-def test_news_editor_user_get(editor_session, base_url, test_news):
-    """Regular user can see news"""
-    response_news_list = editor_session.get(url=f"{base_url}news/")
-    assert response_news_list.status_code == 200, "Ediitor user should have access to news list"
-    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_view_news_editor_user(editor_session, test_news):
+    """Unauthenticated user can see news details"""
     response_news = editor_session.get(url=test_news["test_news_url"])
-    assert response_news.status_code == 200, "Ediitor user should have access to news"
+    assert response_news.status_code == 200, "Unauthenticated user should have access to news"
     assert test_news["test_news_data"]["title"] in response_news.text, "Created news is not accessable"
-def test_news_admin_user_get(admin_session, base_url, test_news):
-    """Regular user can see news"""
-    response_news_list = admin_session.get(url=f"{base_url}news/")
-    assert response_news_list.status_code == 200, "Admin user should have access to news list"
-    assert test_news["test_news_data"]["title"] in response_news_list.text, "Created news is not on news list page"
+def test_view_news_admin_user(admin_session, test_news):
+    """Unauthenticated user can see news details"""
     response_news = admin_session.get(url=test_news["test_news_url"])
-    assert response_news.status_code == 200, "Admin user should have access to news"
+    assert response_news.status_code == 200, "Unauthenticated user should have access to news"
     assert test_news["test_news_data"]["title"] in response_news.text, "Created news is not accessable"
