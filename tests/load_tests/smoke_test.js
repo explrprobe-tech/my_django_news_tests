f8421fc07ccd2cc1de82c9942eef9d664903693d
explrprobe <explrprobe@gmail.com>
2026-06-20 18:57:21 +0700

    fixed smoke_test (was too short for dashboard)

--- tests/load_tests/smoke_test.js
+++ tests/load_tests/smoke_test.js
@@ -8,7 +8,7 @@ export let options = {
         readers: {
             executor: 'constant-vus',
             vus: 2,
-            duration: '5s',
+            duration: '30s',
             exec: 'testReadersFlow'
         }
     }
